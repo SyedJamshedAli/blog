5f29e5c65f4bcc117a8e30d155a67a4680419845
Jamshed <syed_jamshed_ali@yahoo.com>
2024-01-13 22:44:20 +0500

    font issue on mobile browser

--- app/components/PostsList.tsx
+++ app/components/PostsList.tsx
@@ -9,8 +9,8 @@ export default function PostsList({post}: Props) {
     const {id,title,date}=post 
     const formatedDate=getFormattedDate(date); 
   return (
-    <li className="mt-4 text-2xl dark:text-white/90">
-            <Link className="underline hover:text-black/70 dark:hover:text-white" href={`/posts/${id}`}>{title}</Link>
+    <li className="mt-4 text-2xl">
+            <Link className="underline hover:text-black/70" href={`/posts/${id}`}>{title}</Link>
             <br />
             <p className="text-sm mt-1">{formatedDate}</p>
         </li>
