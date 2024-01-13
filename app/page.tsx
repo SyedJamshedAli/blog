dba5a3b42fd3da3667dcc263babc620793a76a8b
Jamshed <syed_jamshed_ali@yahoo.com>
2024-01-13 22:55:58 +0500

    font issue main page title

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Posts from "./components/Posts"
 export default function Home() {
   return (
     <main className="px-6 mx-auto">
-    <p className="mt-12 mb-12 text-3xl text-center dark:text-white">
+    <p className="mt-12 mb-12 text-3xl text-center">
       Hello and Welcome 👋&nbsp;
       <span className="whitespace-nowrap">
         I&apos;m <span className="font-bold">Jam!</span>.
